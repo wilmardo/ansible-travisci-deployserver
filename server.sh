10c6295328d1147b781d62526b24e9bc604bbb94
wilmardo <wilmar94do@hotmail.com>
2017-08-29 00:50:47 +0200

    Added auto pull and update of roles from Github

--- server.sh
+++ server.sh
@@ -4,21 +4,32 @@
 # git clone -n --depth 1 https://github.com/wilmardo/ansible-travisci-deployserver.git .
 # git checkout HEAD server.sh
 
-lansible_path=(/root/LANsible/)
+lansible_path=/opt/deploy/LANsible/
+roles_path=/opt/deploy/roles/
 playbook_paths=($lansible_path/plays/*.yml)
+log_path=/opt/deploy/deployserver/
 
 errorcheck() {
-    typeset cmnd="$*"
+    typeset cmd="$*"
     typeset ret_code
 
-    echo cmnd=$cmnd
-    eval $cmnd
+    echo cmd=$cmd
+    eval $cmd
     ret_code=$?
     if [ $ret_code != 0 ]; then
-        printf "%(%F %T)T Error : ['$ret_code'] when executing command: '$cmnd' \n" -1 >> deploy-error.log
+        printf "%(%F %T)T Error : ['$ret_code'] when executing command: '$cmd' \n" -1 >> $log_path/deploy-error.log
         exit $ret_code
     else
-         printf "%(%F %T)T Successful command: '$cmnd' \n" -1 >> deploy.log
+         printf "%(%F %T)T Successful command: '$cmd' \n" -1 >> deploy.log
+    fi
+}
+
+pull_repository() {
+    typeset cmd="cd $roles_path/ansible-role-$1"
+    if $cmd; then
+        cd $roles_path/ansible-role-$1 && git pull
+    else
+        cd $roles_path && git clone --depth 1 https://github.com/wilmardo/ansible-role-$1.git wilmardo.$1
     fi
 }
 
@@ -32,6 +43,7 @@ ncat -lk 56789 | while IFS=, read -r -a p
 do
 	case "${playbooks[@]}" in  
 	   *"$p"*)
+	        pull_repository $p
             command="ansible-playbook $lansible_path/plays/$p.yml"
 		    errorcheck $command
 	   ;; 
