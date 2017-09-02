f6a6daa91a4a02f6292c177b11d3d452469f7f5c
wilmardo <wilmar94do@hotmail.com>
2017-09-02 22:30:12 +0200

    Added source to test.sh

--- tests/test.sh
+++ tests/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-../server.sh
+source ../server.sh
 
 while ! nc -z localhost 56789; do # check if deployserver is listening
   sleep 0.1 # wait for 1/10 of the second before check again
 done
 
-../client.sh localhost
+source ../client.sh localhost
