b81dce7122e9dc6afa1ab11e918c80d7e59600da
wilmardo <wilmar94do@hotmail.com>
2017-09-03 16:56:17 +0200

    Better format in testing, removed wrong curl login

--- client.sh
+++ client.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 #TODO: Make one curl request and extract http code to prevent inconsistencies
+url=$1
 role=$(printenv TRAVIS_REPO_SLUG | rev | cut -d "-" -f1 | rev) #reverse cut leaves last part of repo name which is role name
 
+echo "Received url: $url"
 echo "Received role: $role"
 if [ "$DEPLOY_CREDENTIALS" ]; then
     echo "DEPLOY_CREDENTIALS defined, sending basic auth request"
-    http_status=$(curl -u $DEPLOY_CREDENTIALS -s -o /dev/null -I -w "%{http_code}" -L $1/?$role)
-    curl -v -u $DEPLOY_CREDENTIALS $1/?$role
+    curl -v -u $DEPLOY_CREDENTIALS $url/?$role
+    http_status=$(curl -u $DEPLOY_CREDENTIALS -s -o /dev/null -I -w "%{http_code}" -L $url/?$role)
 else
     echo "DEPLOY_CREDENTIALS undefined, sending request"
-    http_status=$(curl -s -o /dev/null -I -w "%{http_code}" -L $1/?$role)
-    curl -v -u $DEPLOY_CREDENTIALS $1/?$role
+    curl -v $url/?$role
+    http_status=$(curl -s -o /dev/null -I -w "%{http_code}" -L $url/?$role)
 fi
 
 echo "HTTP status: $http_status"
