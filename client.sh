96504be4c10e3417e5160c69195903b5b35023b7
wilmardo <wilmar94do@hotmail.com>
2017-09-03 16:18:10 +0200

    Fixed if statement in client.sh

--- client.sh
+++ client.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 role=$(printenv TRAVIS_REPO_SLUG | rev | cut -d "-" -f1 | rev) #reverse cut leaves last part of repo name which is role name
 
-if [ -z "$DEPLOY_CREDENTIALS" ]; then
+if [ "$DEPLOY_CREDENTIALS" ]; then
     http_status=$(curl -u $DEPLOY_CREDENTIALS --silent -ls -w "%{http_code}" -L $1/?$role -o /dev/null)
 else
     http_status=$(curl --silent -ls -w "%{http_code}" -L $1/?$role -o /dev/null)
