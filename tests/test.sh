f7faee34adf5c574ddfe96d38718f2234fadf8f4
wilmardo <wilmar94do@hotmail.com>
2017-09-03 15:46:17 +0200

    Edits in test.sh

--- tests/test.sh
+++ tests/test.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-./server.sh
+echo "Server starting"
+source server.sh
 
 while ! nc -z localhost 56789; do # check if deployserver is listening
   sleep 0.1 # wait for 1/10 of the second before check again
 done
+echo "Server started"
 
-./client.sh "localhost"
+echo "Sending request"
+source client.sh "localhost"
