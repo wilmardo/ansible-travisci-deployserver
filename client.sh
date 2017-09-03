ec3a02e7cb5a3830d0e8d2c7f11f9250ee760e73
wilmardo <wilmar94do@hotmail.com>
2017-09-03 16:48:21 +0200

    More verbose testing

--- client.sh
+++ client.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
+#TODO: Make one curl request and extract http code to prevent inconsistencies
 role=$(printenv TRAVIS_REPO_SLUG | rev | cut -d "-" -f1 | rev) #reverse cut leaves last part of repo name which is role name
 
+echo "Received role: $role"
 if [ "$DEPLOY_CREDENTIALS" ]; then
-    http_status=$(curl -u $DEPLOY_CREDENTIALS --silent -ls -w "%{http_code}" -L $1/?$role -o /dev/null)
+    echo "DEPLOY_CREDENTIALS defined, sending basic auth request"
+    http_status=$(curl -u $DEPLOY_CREDENTIALS -s -o /dev/null -I -w "%{http_code}" -L $1/?$role)
+    curl -v -u $DEPLOY_CREDENTIALS $1/?$role
 else
-    http_status=$(curl --silent -ls -w "%{http_code}" -L $1/?$role -o /dev/null)
+    echo "DEPLOY_CREDENTIALS undefined, sending request"
+    http_status=$(curl -s -o /dev/null -I -w "%{http_code}" -L $1/?$role)
+    curl -v -u $DEPLOY_CREDENTIALS $1/?$role
 fi
 
+echo "HTTP status: $http_status"
 if [[ $http_status == 200 ]]; then
     exit 0
 else
