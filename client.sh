cac742c98a278753bbf2c7940fbf6ef6d76d2edf
wilmardo <wilmar94do@hotmail.com>
2017-09-03 15:07:02 +0200

    added http code check and env var for deployment

--- client.sh
+++ client.sh
@@ -2,7 +2,13 @@
 role=$(printenv TRAVIS_REPO_SLUG | rev | cut -d "-" -f1 | rev) #reverse cut leaves last part of repo name which is role name
 
 if [ -z "$DEPLOY_CREDENTIALS" ]; then
-    curl -u $DEPLOY_CREDENTIALS $1/?$role
+    http_status=$(curl -u $DEPLOY_CREDENTIALS --silent -ls -w "%{http_code}" -L $1/?$role -o /dev/null)
 else
-    curl $1/?$role
+    http_status=$(curl --silent -ls -w "%{http_code}" -L $1/?$role -o /dev/null)
+fi
+
+if [[ $http_status == 200 ]]; then
+    exit 0
+else
+    exit 1
 fi
--- server.sh
+++ server.sh
@@ -12,7 +12,7 @@ coproc nc { nc -lk 56789; }
 while [[ $nc_PID ]] && IFS= read -ru ${nc[0]} line; do
     if [[ $line == "GET"* ]]; then
         command=$(echo $line | cut -d ' ' -f 2 | cut -c 3-) #split on space leaves /?role then cut first 2 chars off
-        deploy/$DEPLOY_SCRIPT_NAME $nc $command
+        deploy_scripts/$DEPLOY_SCRIPT_NAME $nc $command
     fi
 done
 
